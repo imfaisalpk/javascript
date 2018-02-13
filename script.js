00a6a27d94b6ca6dec65ff42d9800128eae223bc
Muhammad Faisal <mfaisalpk@outlook.com>
2018-02-13 10:34:42 +0500

    Swictes Done

---
+++ script.js
@@ -0,0 +1,82 @@
+// <!--
+
+function hello(){
+    alert("Hello Faisal Baby!");
+}
+
+var a = 10;
+var b = 5;
+
+switch(a){
+    case 5:
+        alert("value is 5");
+        break;
+    case 10:
+        alert('value is 10');
+        break;
+    default:
+        alert("default");
+        break;
+}
+
+
+
+// var foo = a>b ? 'yes':'no';
+// console.log(foo);
+
+
+// if(null){
+//     console.log("Yes!");
+// }
+
+
+// var foo =    "Long Live Pakistan";
+// var soo = "and Karachi";
+// console.log(foo+" "+soo);
+
+// foo = '2'
+// var i = 1;
+// console.log(+foo+i);
+// var j = ++i;
+// var k = i++;
+// var m = i;
+
+// console.log("j: ",j);
+// console.log("k: ",k);
+// console.log("m: ",m);
+
+// function write(){
+//     console.log(yes);
+// }
+
+// var yes = 10;
+// var no = 0;
+// var other = 3;
+
+// if(yes < other){
+//     console.log("inside if");
+// }
+// else if (yes > other){
+//     console.log("inside else if")
+// }
+// else{
+//     console.log("inside else");
+// }
+
+// console.log(yes>no);
+
+// no || write();
+
+
+// console.log(yes||no);
+// console.log(yes||other);
+// console.log(other||no);
+
+// console.log(yes&&other);
+// console.log(yes&&no);
+// console.log(other&&no);
+
+
+// document.write("Hello Pakistan!");
+
+//--> 
