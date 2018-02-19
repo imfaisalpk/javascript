eafc32237ec0d08fd25e113a6a3e831144c15ef7
Muhammad Faisal <mfaisalpk@outlook.com>
2018-02-19 12:15:59 +0500

    Strings Done

--- new.js
+++ new.js
@@ -1,9 +1,87 @@
 
-var name = "mfaisal";
-var name;
+var str = "mfaisal";
+var arriya = str.split("");
+console.log(arriya);
+
+// console.log(str.concat(":","genius"))
+
+// console.log(str.toLowerCase())
+
+// var replaced = str.replace(/faisal/g,"ali");
+
+// console.log("replaced: "+replaced);
 
 
-document.getElementById("demo").innerHTML=name;
+// var len = str.length;
+
+// var index = str.search("is");
+
+// console.log("index: "+index);
+
+// var part = str.substr(index);
+
+// console.log("part: "+part);
+
+
+
+function displayDate(){
+    document.getElementById("btn_click").innerHTML = Date();
+}
+
+
+
+// function test(){
+//     name = "mfaisal";
+// }
+// var name="mfaisal";
+// function justTest(){
+//     window.alert(window.name);
+// }
+
+
+// test();
+
+
+
+// var car = {
+//     name:"corolla",
+//     company:"toyota",
+//     made:2016,
+//     drive:function(){
+//         return this.name+" is driving now.";
+//     } 
+// }
+
+// console.log(car.drive());
+
+
+
+// function testFunction(a,b){
+//     return a*b;
+// }
+
+// document.getElementById("demo").innerHTML=testFunction(3,4);
+
+// var value = testFunction(3,2);
+// console.log(value);
+
+
+// var listItem = ["mfaisal","ali","amad",1];
+// var objectItem = {name:"mfaisal",occupation:"engineer"};
+// var value = (null===undefined);
+
+
+// var a = 5;
+// var b = 6;
+// var c = (a==b);
+
+// var temp = "mfaisal XC60";
+
+// var name = "mfaisal";
+// var name;
+
+// console.log(typeof(function f(){}));
+// document.getElementById("demo").innerHTML=type(x);
 // document.getElementById("demo").style.fontSize="20px";
 // // document.getElementById("demo").style.display = "none";\
 // document.write("Pakistan Zindabad!");
